dfeec1e204bd6883a91605ea1eefbf219b208d0c
viktorstrate <viktorstrate@gmail.com>
2017-09-30 15:28:03 +0200

    Slideshow now works

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,31 @@
-import babel from 'rollup-plugin-babel';
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+
+let production = process.env.NODE_ENV === 'production'
+
+let plugins = [
+  babel(),
+  resolve({
+    browser: true
+  }),
+  replace({
+    'process.env.NODE_ENV': production ? JSON.stringify('production') : JSON.stringify('development')
+  }),
+  commonjs()
+]
+
+if (production) {
+  plugins.push(uglify())
+}
 
 export default {
-    input: 'src/main.js',
-    output: {
-        file: 'public/bundle.js',
-        format: 'iife'
-    },
-    plugins: [
-        babel()
-    ]
-};
+  input: 'src/main.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'iife'
+  },
+  plugins
+}
--- src/instagram-fetcher.js
+++ src/instagram-fetcher.js
@@ -3,10 +3,14 @@
  * to get the latest images for a specified hashtag from Instagram
  */
 
-import
+import classnames from 'classnames'
 
+const HASHTAG = 'robots'
+const SLIDESHOW_SPEED = 10 * 1000
 
-const HASHTAG = 'sitcomp'
+let imageElements = []
+let currentImage = 0;
+let slideshowInterval = null
 
 window.fetch(`https://qpqp.dk/instagram_hashtag_images.php?hashtag=${HASHTAG}`)
   .then(res => res.json())
@@ -17,4 +21,35 @@ window.fetch(`https://qpqp.dk/instagram_hashtag_images.php?hashtag=${HASHTAG}`)
 function loadImages (images) {
   console.log('Loading instagram images', images)
 
+  const livescore = document.getElementById('images')
+  livescore.innerHTML = ''
+  imageElements = []
+
+  for (let img of images) {
+    if (img === null) continue
+
+    const imgElm = document.createElement('img')
+    imgElm.src = img.display_src
+    imgElm.className = classnames('image')
+
+    imageElements.push(imgElm)
+
+    livescore.appendChild(imgElm)
+  }
+
+  imageElements[currentImage].className = classnames('image', 'shown')
+
+  startSlideshow();
+}
+
+function startSlideshow () {
+  window.clearInterval(slideshowInterval)
+  slideshowInterval = window.setInterval(() => {
+    currentImage = currentImage >= imageElements.length - 1 ? 0 : currentImage + 1
+
+    for (let i = 0; i < imageElements.length; i++) {
+      imageElements[i].className = classnames('image', { shown: i === currentImage })
+    }
+
+  }, SLIDESHOW_SPEED)
 }
